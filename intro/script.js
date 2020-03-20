1a78defb15eecc5a8575900818ac2585915bfe38
Dani de Haard <danidehaard@hotmail.com>
2020-03-20 16:40:43 +0100

    Initial commit

---
+++ intro/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+
+alert("Nice to meet you!, " + firstName + " " + lastName);
+console.log("Nice to meet you!, " + firstName + " " + lastName);
